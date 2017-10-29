99b50315b43e00a5879a2eaec3b8855ecbacecea
Diego Campos <diego.campos@zarpsystem.com.br>
2017-10-29 17:48:45 -0200

    Ajustado model e migration de Transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'description', 'prince', 'prince_paid', 'due_date', 'payment_date','type','source_id','user_id','category_id',
+    ];
 }
--- database/migrations/2017_10_29_184954_create_transactions_table.php
+++ database/migrations/2017_10_29_184954_create_transactions_table.php
@@ -15,6 +15,26 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->longText('description')->nullable();
+
+            $table->decimal('prince', 6, 2)->default(0);
+            $table->decimal('prince_paid', 6, 2)->default(0);
+
+            $table->dateTime('due_date');
+            $table->dateTime('payment_date');
+
+            $table->enum('type', ['Dinheiro', 'Cartão de Crédito', 'Conta Corrente', 'Empréstimo', 'Transferência']);
+
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->integer('source_id')->unsigned();
+            $table->foreign('source_id')->references('id')->on('sources');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
