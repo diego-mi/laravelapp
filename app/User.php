ee077990af5998a9554c71a229253c3286ff24af
Diego Campos <diego.campos@zarpsystem.com.br>
2017-10-29 00:16:14 -0200

    Adicionado relacao user 1x1 category  em user model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the category record associated with the user.
+     */
+    public function category()
+    {
+        return $this->hasOne('App\Category');
+    }
 }
