7692e06f6673256325598259bcfd457e2cb3da35
Diego Campos <diego.campos@zarpsystem.com.br>
2017-10-29 04:47:53 -0200

    adicionado campos fillable no model Source

--- app/Source.php
+++ app/Source.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Source extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'user_id', 'initial_balance', 'current_balance',
+    ];
 }
